40d35a5657595f4f74bfdba076fe2583ceff8e9b
qilianshan <qilianshan@xiaomi.com>
2019-12-17 10:41:45 +0800

    volatile
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

---
+++ src/main/java/Volatile/VolatileTest.java
@@ -0,0 +1,40 @@
+package Volatile;
+
+import java.util.concurrent.TimeUnit;
+
+public class VolatileTest {
+
+	public static int index = 0;
+
+	private static final int MAX = 5;
+
+	public static void main(String[] args) {
+		Thread readThread = new Thread(() -> {
+			int currentIndex = 0;
+			while (index <= MAX) {
+				if (index - currentIndex == 1) {
+					System.out.println(index);
+					currentIndex++;
+				}
+			}
+		});
+		readThread.setName("readThread");
+
+		Thread updateThread = new Thread(() -> {
+			while (index <= MAX) {
+				index++;
+				try {
+					System.out.println("update to:" + index);
+					TimeUnit.SECONDS.sleep(2);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		updateThread.setName("updateThread");
+
+		readThread.start();
+		updateThread.start();
+	}
+
+}
